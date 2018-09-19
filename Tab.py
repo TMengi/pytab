4c4b3fa6f85a2795659f8df4e91144103a5fb160
Tyler McCown <tylermccown@engineering.ucla.edu>
2018-09-18 20:47:57 -0700

    added regex to parse the tuning for a Tab

--- Tab.py
+++ Tab.py
@@ -5,6 +5,10 @@ The main object for the module
 __author__ = "Tyler McCown"
 __date__ = "18 September 2018"
 
+import re
+
+tuning_regex = re.compile("\w#?")
+
 
 class Tab(object):
 
@@ -15,5 +19,5 @@ class Tab(object):
         :param tuning: string representing open notes
         :param capo: optional integer for capo
         """
-        self.tuning = tuning
+        self.tuning = tuning_regex.findall(tuning)
         self.capo = capo
